48960b2a4fd81299960eb1eb9bba85141c93ccfe
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-08-31 17:32:16 +0100

    Update DOM logic

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const gameLogic = (function(){
   Player.playerTwo;
 
   const currentPlayer = () => {
-    
+     for (let i = 0; i<10 ; i++){
     if(playerOne.turn === true){
        playerTwo.turn = false;
        value= playerOne.marker;
@@ -75,6 +75,7 @@ const gameLogic = (function(){
       value = playerTwo.marker;
        rows[columns]= value;
     };
+  }
   }
 
   function checkWin (){
@@ -94,25 +95,49 @@ const gameLogic = (function(){
 
         
       }
-      rows[0].every(isMatchedX)
+      board[rows[0]].every(isMatchedX);
+      board[rows[1]].every(isMatchedX);
+      board[rows[2]].every(isMatchedX);
+      board[0].every(isMatchedX);
+      board[1].every(isMatchedX);
+      board[2].every(isMatchedX);
+      board[0][2] && board[1][1]&& board[2][0].every(isMatchedX);
+      board[0][0] && board[1][1]&& board[2][2].every(isMatchedX);
+      
+    return winner;
   }
 
-   function checkDraw(){};
+   function checkDraw(){
+     let draw= false;
+      if (gameBoard.board !== ''){
+         draw = true;
+         console.log("it's a draw")
+      } 
+
+      return draw;
+
+   };
 
 
-   function gameOver(){};
+   function gameOver(){
+
+    if(gameBoard.board.every( value => value !== '')){
+      
+      console.log("it's game-over")
+   }
 
 
 
 
 
-
+  }
 
 
    return {gameActive, currentPlayer, checkWin, checkDraw, gameOver};
 })()
 
  // DOM Logic
+ const tabs = document. querySelectorAll(".cell")
 
  // display game array
 
@@ -124,64 +149,4 @@ const gameLogic = (function(){
 
  // modal for prompt player name
  
-   function winningPlayer(){
-  if(row1[0]&& row1[1] && row1[2] === "X" ||
-     row2[0]&& row2[1] && row2[2] === "X" ||
-     row3[0]&& row3[1] && row3[2] === "X" ||
-     row1[0]&& row2[1] && row3[2] === "X" ||
-     row3[0]&& row2[1] && row1[2] === "X"
-  ){
-    console.log(`${playerOne.name} has won the game`)
-    playerOne.score += 1;
-
-  }else if (row1[0]&& row1[1] && row1[2] === "O" ||
-            row2[0]&& row2[1] && row2[2] === "O" ||
-            row3[0]&& row3[1] && row3[2] === "O" ||
-            row1[0]&& row2[1] && row3[2] === "O" ||
-            row3[0]&& row2[1] && row1[2] === "O"
-
-         ){ console.log(`${playerTwo.name} has won the game`);
-         playerTwo.score += 1;
-
-           
-   }
-
- return{ winningPlayer}
-}
-
-
-
-function playGame(){
-   
-    
-    switchTurn(playerOne,playerTwo);
-   
-let token = Player.marker;
- let instance = row1|| row2|| row3;
-
-  return function play (newState){
-    newState = instance.push(token)
-    return newState;
-  }
-instance.push(token);
-
-playerOne.turn=true;
-    console.log(`player has played ${x} in ${y}`)
-    console.log(`${y = token}`)
-}playGame()
-
-
-
-function gameControl(){
- winningPlayer()
-
- if(Player.score ===1){
-    row1().reset();
-    row2().reset();
-    row3().reset();
-
-    playGame()
- }
-
-  console.log ( Player.name )
-}gameControl();
+  
